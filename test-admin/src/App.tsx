e4f347b9f47ee458a700b1e427edc706f302f6c6
Nicolas Alarcon <A01783704@tec.mx>
2024-09-09 16:07:11 -0600

    Menu de inclusión

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -9,9 +9,8 @@ import {
   houseDarkTheme,
 } from "react-admin";
 import { Layout } from "./Layout";
-//import { dataProvider } from "./dataProvider";
 import { UserCreate, UserEdit, UserList } from "./users";
-import {PostCreate, DonadoresList} from "./Donaciones";
+import { PostCreate, DonadoresList } from "./Donaciones";
 import { authProv } from "./authProvider";
 import { Dashboard } from "./Dashboard";
 import PostIcon from "@mui/icons-material/Book";
@@ -20,34 +19,28 @@ import CommentIcon from "@mui/icons-material/Comment";
 import PhotoIcon from "@mui/icons-material/Photo";
 import ChecklistIcon from "@mui/icons-material/Checklist";
 import AlbumIcon from "@mui/icons-material/Album";
-import { PageTheme, PageThemeDark } from "./Themes";
 import { CommentCreate, CommentEdit, CommentList } from "./comments";
 import { AlbumCreate, AlbumEdit, AlbumList } from "./albums";
 import { PhotosCreate, PhotosEdit, PhotosList } from "./photos";
 import { TodoCreate, TodoEdit, TodosList } from "./todos";
 import { MyLoginPage } from "./LogIn";
-import { usePermissions } from "react-admin";
-import { permission } from "process";
-import simpleRestProvider from 'ra-data-simple-rest';
-import {basedatos} from './dataprovider'
-
-
+import { basedatos } from './dataprovider';
 
 export const App = () => (
-
-  <Admin  loginPage={MyLoginPage} authProvider={authProv} layout={Layout} dashboard={Dashboard} dataProvider={basedatos} theme={houseLightTheme}
-  darkTheme={houseDarkTheme} /*theme={PageTheme} darkTheme={PageThemeDark}*/> /* The themes that are commented are custom themes, I created, the one being used is one improted*/
-
-    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} icon={UserIcon} ></Resource>
-    <Resource name="donaciones" list={DonadoresList} /*edit={PostEdit}*/ create={PostCreate} icon={PostIcon}></Resource>
-
-    <Resource name="comments" list={CommentList} edit={CommentEdit} create={CommentCreate}icon={CommentIcon}></Resource>
-
-    <Resource name="albums" list={AlbumList} edit={AlbumEdit} create={AlbumCreate} icon={AlbumIcon}></Resource>
-
-    <Resource name="photos" list={PhotosList} edit={PhotosEdit} create={PhotosCreate} icon={PhotoIcon}></Resource>
-
-    <Resource name="todos" list={TodosList} edit={TodoEdit} create={TodoCreate} icon={ChecklistIcon}></Resource>
-
+  <Admin
+    loginPage={MyLoginPage}
+    authProvider={authProv}
+    layout={Layout}
+    dashboard={Dashboard}
+    dataProvider={basedatos}
+    theme={houseLightTheme}
+    darkTheme={houseDarkTheme}
+  >
+    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} icon={UserIcon} />
+    <Resource name="donaciones" list={DonadoresList} create={PostCreate} icon={PostIcon} />
+    <Resource name="comments" list={CommentList} edit={CommentEdit} create={CommentCreate} icon={CommentIcon} />
+    <Resource name="albums" list={AlbumList} edit={AlbumEdit} create={AlbumCreate} icon={AlbumIcon} />
+    <Resource name="photos" list={PhotosList} edit={PhotosEdit} create={PhotosCreate} icon={PhotoIcon} />
+    <Resource name="todos" list={TodosList} edit={TodoEdit} create={TodoCreate} icon={ChecklistIcon} />
   </Admin>
 );
